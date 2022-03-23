d6ce342728384fc8ddde563749529d830e18d3b3
HailemichaelHa <Hailemichaelteklu@gmail.com>
2022-03-23 15:50:17 -0500

    backend first commit

---
+++ src/test/java/com/project2springbootrestspringdataers/Project2SpringBootRestSpringdataErsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2springbootrestspringdataers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2SpringBootRestSpringdataErsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
